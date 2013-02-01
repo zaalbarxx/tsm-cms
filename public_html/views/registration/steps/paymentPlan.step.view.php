d160837fed00bfd272ae6e963e35377c571c0432
jeremymlane <jeremymlane>
2013-02-01 05:15:31 +0000

    adjust wording on choose payment plan screen for parents
    git-svn-id: https://subversion.assembla.com/svn/tsm-cms/branches/1@47 f221f11d-c50b-394f-a1f7-493ed0d75264

--- public_html/views/registration/steps/paymentPlan.step.view.php
+++ public_html/views/registration/steps/paymentPlan.step.view.php
@@ -3,7 +3,7 @@
 
     <h1>Choose Payment Plan</h1>
 
-    <p style="text-align: center;">Please choose a payment plan for both your tuition and registration fees.</p>
+    <p style="text-align: center;">Please choose a payment plan for each of the fee types below.</p>
   <?php if (isset($errorMessage)) {
   echo "<p style='text-align: center; color: red; font-weight: bold'>".$errorMessage."</p>";
 } ?>
