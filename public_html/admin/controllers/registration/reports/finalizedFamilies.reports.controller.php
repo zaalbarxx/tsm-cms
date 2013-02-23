808e860440860187fd4ecc0fd40031972b4c4598
jeremymlane <jeremymlane>
2013-02-23 04:16:09 +0000

    fixed finalized/unfinalized families reports to show the correct families. They were reveresed.
    git-svn-id: https://subversion.assembla.com/svn/tsm-cms/branches/1@81 f221f11d-c50b-394f-a1f7-493ed0d75264

--- public_html/admin/controllers/registration/reports/finalizedFamilies.reports.controller.php
+++ public_html/admin/controllers/registration/reports/finalizedFamilies.reports.controller.php
@@ -2,7 +2,7 @@
 $families = $currentCampus->getFamilies();
 if (isset($families)) {
   foreach ($families as $family) {
-    if ($family['current_step'] == 0) {
+    if ($family['current_step'] != 0) {
       unset($families[$family['family_id']]);
     }
 
