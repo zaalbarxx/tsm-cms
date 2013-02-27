6a34babcbdf8a1d03e428307037e596473f02248
jeremymlane <jeremymlane>
2013-02-27 05:37:00 +0000

    add ability to select a quickbook payment method for paypal payments to use in quickbooks
    git-svn-id: https://subversion.assembla.com/svn/tsm-cms/branches/1@88 f221f11d-c50b-394f-a1f7-493ed0d75264

--- public_html/admin/controllers/registration/quickbooks/connected.quickbooks.controller.php
+++ public_html/admin/controllers/registration/quickbooks/connected.quickbooks.controller.php
@@ -3,6 +3,9 @@ if (isset($saveQuickbooksStatus)) {
   $currentCampus->saveCampus();
   $message = "Your Quickbooks status has been successfully saved.";
 }
+
+$service = new QuickBooks_IPP_Service_PaymentMethod();
+$paymentMethods = $service->findAll($quickbooks->Context, $quickbooks->creds['qb_realm']);
 $campusInfo = $currentCampus->getInfo();
 
 //$ItemService = new QuickBooks_IPP_Service_Item();
--- public_html/admin/views/registration/quickbooks/connected.quickbooks.view.php
+++ public_html/admin/views/registration/quickbooks/connected.quickbooks.view.php
@@ -2,30 +2,43 @@
 require_once(__TSM_ROOT__."admin/views/registration/sidebar.view.php");
 ?>
 <div class="contentWithSideBar">
-    <h1>Quickbooks Connection</h2>
-      <?php if (isset($message)) { ?>
-            <p style="text-align: center; color: red; font-weight: bold;"><?php echo $message; ?></p>
-        <?php } ?>
-        <p>This campus has been connected to Quickbooks. If you would like to use Quickbooks with this campus, you must
-            enable it below.</p>
+    <h1>Quickbooks Connection</h1>
+  <?php if (isset($message)) { ?>
+    <p style="text-align: center; color: red; font-weight: bold;"><?php echo $message; ?></p>
+  <?php } ?>
+    <p>This campus has been connected to Quickbooks. If you would like to use Quickbooks with this campus, you must
+        enable it below.</p>
 
-        <form action="" method="POST">
-            <label for="quickbooks_enabled" style="width: 160px;">Quickbooks Status:</label> <select
-                name="quickbooks_enabled">
-            <option value="0" <?php if ($campusInfo['quickbooks_enabled'] == 0) {
-              echo "selected=selected";
-            } ?>>Disabled
-            </option>
-            <option value="1" <?php if ($campusInfo['quickbooks_enabled'] == 1) {
-              echo "selected=selected";
-            } ?>>Enabled
-            </option>
-        </select>
-            <input type="hidden" name="saveQuickbooksStatus" value="1"/>
-            <input type="submit" class="submitButton" value="Save Status"/>
-        </form>
-        <p style="text-align: center;">
-        </p>
+    <form action="" method="POST">
+        <label for="quickbooks_enabled" style="width: 160px;">Quickbooks Status:</label> <select
+            name="quickbooks_enabled">
+        <option value="0" <?php if ($campusInfo['quickbooks_enabled'] == 0) {
+          echo "selected=selected";
+        } ?>>Disabled
+        </option>
+        <option value="1" <?php if ($campusInfo['quickbooks_enabled'] == 1) {
+          echo "selected=selected";
+        } ?>>Enabled
+        </option>
+    </select><br/>
+        <label for="qb_paypal_payment_method_id" style="width: 200px;">PayPal Payment Method:</label><select
+            name="qb_paypal_payment_method_id">
+      <?php
+      foreach ($paymentMethods as $method) {
+        ?>
+          <option value="<?php echo $method->getId(); ?>" <?php if ($campusInfo['qb_paypal_payment_method_id'] == $method->getId()) {
+            echo "selected=selected";
+          } ?>><?php echo $method->getName(); ?>
+          </option>
+        <?php
+      }
+      ?>
+    </select><br/><br/>
+        <input type="hidden" name="saveQuickbooksStatus" value="1"/>
+        <input type="submit" class="submitButton" value="Save Configuration"/>
+    </form>
+    <p style="text-align: center;">
+    </p>
 </div>
 
 
