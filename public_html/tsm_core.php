1a232dd5d0a133e61b3ea40fa257184fd2d01dca
jeremymlane <jeremymlane>
2013-03-27 22:05:38 +0000

    add mpdf to project
    git-svn-id: https://subversion.assembla.com/svn/tsm-cms/branches/1@118 f221f11d-c50b-394f-a1f7-493ed0d75264

--- public_html/tsm_core.php
+++ public_html/tsm_core.php
@@ -4,4 +4,5 @@ require_once(__TSM_ROOT__.'classes/tsm.class.php');
 require_once(__TSM_ROOT__.'classes/db.class.php');
 require_once(__TSM_ROOT__.'classes/adminUser.class.php');
 require_once(__TSM_ROOT__.'classes/website.class.php');
+require_once(__TSM_ROOT__.'includes/mpdf/mpdf.php');
 ?>
