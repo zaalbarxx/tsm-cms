e0ae9b1ff28681ab580d63494c46017f22654a6d
Jeremy Lane <jlane@veritasproductions.net>
2013-05-10 21:21:16 -0700

    add quickbooks class option to payment plan invoices and credit edit screen

---
+++ db/schema_migration/v.01.004-v.01.005.sql
@@ -0,0 +1,5 @@
+ALTER TABLE  `tsm_reg_fee_payment_plans` ADD  `qb_invoice_class_id` VARCHAR( 255 ) NOT NULL AFTER  `num_invoices`;
+
+ALTER TABLE  `tsm_reg_fee_payment_plans` ADD  `qb_credit_class_id` VARCHAR( 255 ) NOT NULL AFTER  `num_invoices`;
+
+UPDATE  `tsm_config` SET  `config_value` =  '.01.005' WHERE  `tsm_config`.`config_id` =1;
