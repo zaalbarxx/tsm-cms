b120a95e7beb78ebaa4fba5d9fc8f2e27646cf6d
Jeremy Lane <jlane@veritasproductions.net>
2013-06-20 20:32:50 -0700

    updated course rosters to include period

--- public_html/modules/registration/BackEnd/controllers/course/roster.course.controller.php
+++ public_html/modules/registration/BackEnd/controllers/course/roster.course.controller.php
@@ -2,6 +2,20 @@
 $course = new TSM_REGISTRATION_COURSE($course_id);
 $courseInfo = $course->getInfo();
 $courseStudents = $course->getEnrolledStudents();
+$periods = $course->getPeriods();
+foreach($courseStudents as $id=>$student){
+  $courseStudents[$id]['period'] = $reg->displayPeriod($periods[$student['course_period_id']]);
+}
+
+$periods = $course->getPeriods();
+foreach($periods as $period){
+  foreach($courseStudents as $student){
+    if($student['course_period_id'] == $period['course_period_id']){
+      $periods[$period['course_period_id']]['students'][] = $student;
+    }
+  }
+  $periods[$period['course_period_id']]['students'];
+}
 
 if (isset($downloadCSV)) {
   $tsm->arrayToCSV($courseStudents, $courseInfo['name']." - Roster");
