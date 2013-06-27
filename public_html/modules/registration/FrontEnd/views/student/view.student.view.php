513d5dd85eb37792ac7bcc32c72395d06650f6da
Jeremy Lane <jlane@veritasproductions.net>
2013-06-27 13:28:55 -0700

    added confirmation message to front end editing.

--- public_html/modules/registration/FrontEnd/views/student/view.student.view.php
+++ public_html/modules/registration/FrontEnd/views/student/view.student.view.php
@@ -31,7 +31,7 @@ require_once(__TSM_ROOT__."modules/registration/FrontEnd/views/sidebar.view.php"
 				<span class="buttons">
 					<!--<a href="#" class="reviewButton" title="Review This Program"></a>
 					<a href="#" class="editButton" title="Edit This Student"></a>-->
-					<a href="index.php?mod=registration&ajax=unenrollFromProgram&student_id=<?php echo $student_id; ?>&program_id=<?php echo $program['program_id']; ?>" class="deleteButton" title="Unenroll From This Program"></a>
+					<a href="index.php?mod=registration&ajax=unenrollFromProgram&student_id=<?php echo $student_id; ?>&program_id=<?php echo $program['program_id']; ?>" class="deleteButton" data-tsm-confirm-message="Are you sure that you want to unenroll <?php echo $studentInfo['first_name']; ?> from <?php echo $program['name']; ?>?" title="Unenroll From This Program"></a>
 				</span>
 
                 <div class="itemDetails" style="display: block;">
@@ -49,7 +49,7 @@ require_once(__TSM_ROOT__."modules/registration/FrontEnd/views/sidebar.view.php"
                       $i = 1;
                       if ($program['courses']) {
                         foreach ($program['courses'] as $course) {
-                          echo "<tr><td>".$i.". ".$course['name']."</td><td>".$tsm->intToDay($course['day']).". ".date("g:ia", strtotime($course['start_time']))." - ".date("g:ia", strtotime($course['end_time']))."</td><td>".$course['teacher_name']."</td><td align=center>$".$course['tuition_amount']."</td><td align=center>$".$course['registration_amount']."</td><td><a href=\"index.php?mod=registration&ajax=unenrollFromCourse&course_id=".$course['course_id']."&student_id=".$student_id."&program_id=".$course['program_id']."\" title=\"Unenroll From This Course\" data-tsm-course-id=\"".$course['course_id']."\" data-tsm-program-id=\"".$course['program_id']."\" class=\"deleteButton button\"></a></td></tr>";
+                          echo "<tr><td>".$i.". ".$course['name']."</td><td>".$tsm->intToDay($course['day']).". ".date("g:ia", strtotime($course['start_time']))." - ".date("g:ia", strtotime($course['end_time']))."</td><td>".$course['teacher_name']."</td><td align=center>$".$course['tuition_amount']."</td><td align=center>$".$course['registration_amount']."</td><td><a href=\"index.php?mod=registration&ajax=unenrollFromCourse&course_id=".$course['course_id']."&student_id=".$student_id."&program_id=".$course['program_id']."\" title=\"Unenroll From This Course\" data-tsm-course-id=\"".$course['course_id']."\" data-tsm-program-id=\"".$course['program_id']."\"  data-tsm-confirm-message=\"Are you sure that you want to unenroll ".$studentInfo['first_name']." from ".$course['name']."?\"  class=\"deleteButton button\"></a></td></tr>";
                           $i++;
                         }
                       } else {
@@ -103,17 +103,22 @@ require_once(__TSM_ROOT__."modules/registration/FrontEnd/views/sidebar.view.php"
         }
     });
     $(".deleteButton").click(function () {
-      $.get($(this).attr("href"), function (data) {
-        var response = JSON.parse(data);
-        if (response.alertMessage != null) {
-          alert(response.alertMessage);
-        }
-        if (response.success == true) {
-          window.location.reload();
-        }
-      });
-      return false;
-    });
+      var confirmDelete = confirm($(this).attr("data-tsm-confirm-message"));
+      if(confirmDelete){
+        $.get($(this).attr("href"), function (data) {
+          var response = JSON.parse(data);
+          if (response.alertMessage != null) {
+            alert(response.alertMessage);
+          }
+          if (response.success == true) {
+            window.location.reload();
+          }
+        });
+      }
+        return false;
+
+     });
+
     /*
     $(".showDetails").click(function () {
         if ($(this).html() == "Show Details") {
